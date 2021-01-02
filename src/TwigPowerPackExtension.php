4b56e720c0f683fbb6e8692f6d9b3235256d182a
Bruce <bruce@mediagone.fr>
2021-01-02 16:01:59 +0100

    Add TwigPowerPackExtension class

---
+++ src/TwigPowerPackExtension.php
@@ -0,0 +1,30 @@
+<?php declare(strict_types=1);
+
+namespace Mediagone\Twig\PowerPack;
+
+use Mediagone\Twig\PowerPack\Tags\RequireTokenParser;
+use Twig\Extension\AbstractExtension;
+
+
+final class TwigPowerPackExtension extends AbstractExtension
+{
+    //========================================================================================================
+    //
+    //========================================================================================================
+    
+    public function getName() : string
+    {
+        return 'TwigPowerPackExtension';
+    }
+    
+    
+    public function getTokenParsers() : array
+    {
+        return [
+            new RequireTokenParser(),
+        ];
+    }
+    
+    
+    
+}
